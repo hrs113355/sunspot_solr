e99a1accfccbe3d5e4efc7be5085840eb948d4b5
hrs <hrs113355@gmail.com>
2014-10-07 14:28:06 +0800

    modify conf folder location since there is a new collection1 folder

--- sunspot_solr/lib/sunspot/solr/installer.rb
+++ sunspot_solr/lib/sunspot/solr/installer.rb
@@ -18,7 +18,7 @@ module Sunspot
       end
 
       def execute
-        ConfigInstaller.execute(File.join(@solr_home, 'conf'), @options)
+        ConfigInstaller.execute(File.join(@solr_home, 'collection1', 'conf'), @options)
       end
     end
   end
